85561909147d4c8eaa5a5c094876073167208b51
Mohammad Abdul Basit <abasit00@hotmail.com>
2021-04-28 11:19:40 +0300

    Added the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,24 @@ function openNav() {
   var c=document.getElementById("select");
   var ctx=c.getContext("2d");
   ctx.fillStyle="#FF0000";
+
+
+  var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
+  
+  
